3f3a2e32e2f4b6c102fe9172ee37b641296510a9
talhaozgn <101015459+talhaozgn@users.noreply.github.com>
2023-11-30 19:10:27 -0600

    heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://blooming-bastion-60043-066185deef78.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
