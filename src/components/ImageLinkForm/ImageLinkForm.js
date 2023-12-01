ea463477d72e5df35fdb1057b20d12185f24dc82
talhaozgn <101015459+talhaozgn@users.noreply.github.com>
2023-11-30 19:19:16 -0600

    text edit

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,8 +5,14 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
+
+      <p className='f3'>
+        {'Just copy and face image url and paste it to box below'}
+      </p>
+
+
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
